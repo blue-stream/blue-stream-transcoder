3c880d51c9bd671dd43334d6f1aabcebbf5848cc
SirSaar <saar643@gmail.com>
2019-05-05 10:03:42 +0300

    feat: add prefetch

--- src/transcode/transcode.broker.ts
+++ src/transcode/transcode.broker.ts
@@ -6,25 +6,39 @@ export class TranscodeBroker {
 
     public static async subscribe() {
         rabbit.subscribe(
-        'application',
-        'topic',
-        'transcoder-transcode-queue',
-        'videoService.video.upload.succeeded',
-        async (video: any) => {
-            try {
-                const products: string[] = await TranscodeController.transcode((video as IUploadedVideo).key);
-                const newVideo: ITranscodedVideo  = {
-                    id: (video as IUploadedVideo).id,
-                    thumbnailPath: products[0],
-                    previewPath: products[1],
-                    contentPath: products[2],
-                };
-                rabbit.publish('application', 'topic', 'transcoder.video.transcode.succeeded', newVideo);
-            } catch (error) {
-                rabbit.publish('application', 'topic', 'transcoder.video.transcode.failed', video);
-                throw error;
+            'application',
+            'topic',
+            'transcoder-transcode-queue',
+            'videoService.video.upload.succeeded',
+            async (video: any) => {
+                try {
+                    const products: string[] = await TranscodeController.transcode((video as IUploadedVideo).key);
+                    const newVideo: ITranscodedVideo = {
+                        id: (video as IUploadedVideo).id,
+                        thumbnailPath: products[0],
+                        previewPath: products[1],
+                        contentPath: products[2],
+                    };
+                    rabbit.publish('application', 'topic', 'transcoder.video.transcode.succeeded', newVideo);
+                } catch (error) {
+                    rabbit.publish('application', 'topic', 'transcoder.video.transcode.failed', video);
+                    throw error;
+                }
+            },
+            {
+                consumer: {
+                    noAck: false,
+                },
+                exchange: {
+                    durable: true,
+                },
+                queue: {
+                    durable: true,
+                },
+                channel: {
+                    prefetch: 1,
+                },
             }
-        },
         );
 
     }
